16c775a28fb340b2aab8d2ebe6148d9500e1fcf4
Alex <oleksandr.spynskyi.itis.2018@lpnu.ua>
2021-02-19 15:20:28 +0200

    task complete

--- lab1.py
+++ lab1.py
@@ -1,7 +1,17 @@
+import datetime
+
 print("Hello World!")
-print("Enter your username")
+print("Enter your username: ")
 username = input()
 print("Hello," + username)
-print("Letters in username =" + str(len(username)))
-print("Your date of birth in DD.MM.YY format")
-dob = input()
+print("number of letters in username =" + str(len(username)))
+
+
+birth_date_string = input('Please, enter your birth date in format (YYYY.MM.DD): ')
+year, month, day = map(int, birth_date_string.split('.'))
+birth_date = datetime.date(year, month, day)
+now = datetime.date.today()
+age_days = now - birth_date
+age_years = int(age_days.days/365)
+
+print('Today is', now , ',your age is', age_years, 'years (', age_days, ')')
